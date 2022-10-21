797949cf87e79c9b62eee77a89f6c977d077859b
husna <hpm25@georgetown.edu>
2022-10-21 00:13:33 -0700

    add two methods to identify frequent datetimes

--- event_manager.rb
+++ event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -44,16 +45,18 @@ end
 
 puts 'EventManager initialized.'
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+def open_csv
+  contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+  )
+end
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
-contents.each do |row|
+open_csv.each do |row|
   id = row[0]
   
   name = row[:first_name]
@@ -68,3 +71,41 @@ contents.each do |row|
 
   save_thank_you_letter(id, form_letter)
 end
+
+
+def freq_hour_registered
+  hrs = []
+  contents = open_csv
+  contents.each do |row|
+    hrs.push(Time.strptime(row[:regdate], '%M/%d/%y %k:%M').strftime('%H'))
+  end
+  new_hash = hrs.each_with_object(Hash.new(0)) {|hour, new_hash| new_hash[hour] += 1}
+  hours = []
+  new_hash.each{|key, value| hours.push(key.to_i) if value == new_hash.values.max}
+  return hours
+end
+
+def freq_wkday_registered
+  wkdays = []
+  contents = open_csv
+  contents.each do |row|
+    wkdays.push(Time.strptime(row[:regdate], '%M/%d/%y %k:%M').strftime('%A'))
+  end
+  new_hash = wkdays.each_with_object(Hash.new(0)) {|wkday, new_hash| new_hash[wkday] += 1}
+  new_hash.each{|key, value| return key if value == new_hash.values.max}
+end 
+
+def save_email(email)
+  Dir.mkdir('email') unless Dir.exist?('email')
+
+  filename = "email/data_analysis.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts email
+  end
+end
+
+template_email = File.read('email.erb')
+erb_email = ERB.new template_email
+email = erb_email.result(binding)
+save_email(email)
